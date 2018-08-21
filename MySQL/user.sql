ca1c9e155f03b1b52e57f786461f38e91851aed1
Thomas Kipp <t.kipp@eisvogel-onlnie-software.de>
2018-08-21 12:22:25 +0200

    Vervollständigung - Teil 3

---
+++ MySQL/user.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Erstellungszeit: 21. Aug 2018 um 12:17
+-- Server-Version: 10.1.34-MariaDB
+-- PHP-Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Datenbank: `user`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `benutzer`
+--
+
+CREATE TABLE `benutzer` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) COLLATE utf8_german2_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_german2_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_german2_ci;
+
+--
+-- Daten für Tabelle `benutzer`
+--
+
+INSERT INTO `benutzer` (`id`, `username`, `password`) VALUES
+(44, 'admin', '$2y$10$zTapKxt1cAOag71pcrIkUeA6CCKNmnSOikFm58xuPrR/8uE08/6te'),
+(45, 'tklustig', '$2y$10$YynqRwhkvVWCzrw6tuXUnu1Q3BAfTrniGqNCiiZj.ALL5vwsjNQtS'),
+(47, 'tklustig.thomas@gmail.com', '$2y$10$mmoIHIaqpZa6L.sXnYi8C.DYFjCYuSZRVtyYJPAa9oBowVYZ/JASu');
+
+--
+-- Indizes der exportierten Tabellen
+--
+
+--
+-- Indizes für die Tabelle `benutzer`
+--
+ALTER TABLE `benutzer`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT für exportierte Tabellen
+--
+
+--
+-- AUTO_INCREMENT für Tabelle `benutzer`
+--
+ALTER TABLE `benutzer`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

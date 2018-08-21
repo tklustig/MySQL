42c91d8bf2bd00c647d7090940d61ae0770d4be3
Thomas Kipp <t.kipp@eisvogel-onlnie-software.de>
2018-08-21 12:10:39 +0200

    Vervollständigung - Teil 1

---
+++ MySQL/correct_passsword.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Login Done</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    </head>
+    <body>
+        <div class = "container">
+            <div class="row">
+                <div class="page-header">
+                    <h3><small>Feedback</small></h3>
+                </div>
+                <div class="col-md-12">
+                    <div class="alert alert-info" role="alert">Sehr gut. Die Logindaten wurden geknackt...</div>
+                </div>
+                <div class="col-md-12">
+                    <input type="button" class="btn btn-success" value="Go Back" onclick="window.location.href = 'passwordhash.php'" />
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
